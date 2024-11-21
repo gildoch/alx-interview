2d1a16f7a6aeb22d479b7b17111467cce74328f8
Gildo Chauze <gildochauze@gmail.com>
2024-11-21 11:27:01 +0200

    Change aproach

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,15 +1,31 @@
 def rotate_2d_matrix(matrix):
     """
-    Rotates a given n x n 2D matrix 90 degrees clockwise in-place.
+    Rotates a 2D matrix 90 degrees clockwise in-place.
     Args:
-        matrix: List of lists where each inner list represents a row of the
+        matrix: A list of n lists where each inner list represents a row of the
         matrix.
     """
     n = len(matrix)
-    # Transpose the matrix (swap rows with columns)
-    for i in range(n):
-        for j in range(i + 1, n):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    # Reverse each row to achieve the 90-degree rotation
-    for row in matrix:
-        row.reverse()
+
+    # Process each layer from the outermost to the innermost
+    for layer in range(n // 2):
+        first, last = layer, n - 1 - layer  # Define the boundaries of the
+        current layer
+
+        for i in range(first, last):
+            offset = i - first  # Offset for elements in the current layer
+
+            # Save the top-left element
+            top_left = matrix[first][i]
+
+            # Move bottom-left to top-left
+            matrix[first][i] = matrix[last - offset][first]
+
+            # Move bottom-right to bottom-left
+            matrix[last - offset][first] = matrix[last][last - offset]
+
+            # Move top-right to bottom-right
+            matrix[last][last - offset] = matrix[i][last]
+
+            # Move top-left to top-right
+            matrix[i][last] = top_left
